39afaa007076a73412c20fe77e1b92c41367c7bb
pradeept <pradeep.tarakar17@gmail.com>
2025-12-06 16:46:33 +0530

    feat: add helper functions to models. Register in admin site.
    - Added helper functions like __str__
    - Explored django shell
    - Created admin user and explored admin site

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+from typing import override
 from django.db import models
 
 # Create your models here.
@@ -9,10 +10,25 @@ from django.db import models
 #     address = models.TextField(null=True)
 
 class Question(models.Model):
+    """
+        It’s important to add __str__() methods to your models, not only for your own convenience
+        when dealing with the interactive prompt, but also because objects’ representations are used
+        throughout Django’s automatically-generated admin.
+    """
+    def __str__(self):
+        return self.question_text
+
+    # helper function to later use in views or templates.
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("Date published")
 
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    votes = models.IntegerField(default = 0)
